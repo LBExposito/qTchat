fb435b80d1e6abeb58815a956562d828f99de2a6
Luc Braun-Exposito <luc.braun-exposito@epitech.eu>
2020-01-14 15:07:35 +0100

    init cmakelist + gitignore

---
+++ Network/connection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lexposito on 14/01/2020.
+//
+
+#include "connection.hpp"
---
+++ Network/connection.hpp
@@ -0,0 +1,14 @@
+//
+// Created by lexposito on 14/01/2020.
+//
+
+#ifndef WORKSHOPQT_CONNECTION_HPP
+#define WORKSHOPQT_CONNECTION_HPP
+
+
+class connection {
+
+};
+
+
+#endif //WORKSHOPQT_CONNECTION_HPP
